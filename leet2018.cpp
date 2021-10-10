f6d07bd877501fb99e007cfced982f003cd2e24c
ivy-lv11 <181220038@smail.nju.edu.cn>
2021-10-10 10:41:00 +0800

    leetcode 2018

---
+++ leet2018.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <cstring>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <cmath>
+#include <functional>
+#include <cmath>
+#include <set>
+#include <map>
+#include <assert.h>
+#include <stack>
+#include <unordered_map>
+using namespace std;
+
+
+class Solution {
+public:
+    bool placeWordInCrossword(vector<vector<char>>& board, string word) {
+        
+    }
+};
+
+int main()
+{
+    return 0;
+}
