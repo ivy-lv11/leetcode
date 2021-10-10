18fab4679facdbd19b4ce83038e14ba62f94c574
ivy-lv11 <181220038@smail.nju.edu.cn>
2021-10-10 08:26:45 +0800

    finish leetcode 441

---
+++ leet441.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int total = 0;
+        int k = 0;
+        while(total<n)
+        {
+            k++;
+            total += k;
+        }
+        return k;
+    }
+};
